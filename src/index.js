26634546440071461826b8c8d050451a921abcce
Petar Vorotnikov <petar@vorotnikov.net>
2017-11-04 08:40:45 +0200

    Created DB model. Added docker-compose and dockerfile for creating dev environment

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const path = require('path')
 const express = require('express')
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
-const { logger } = require('./lib');
+const { logger } = require('./lib')
 const api = require('./api')
 
 // create app
 const app = express()
 
 // setup logging
-app.use(morgan('combined', { 'stream': logger.stream }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined', { 'stream': logger.stream }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
     res.send('hello, world!')
---
+++ src/models/index.js
@@ -0,0 +1,131 @@
+const { logger } = require('../lib')
+const nconf = require('nconf')
+const bcrypt = require('bcrypt')
+const uuidv4 = require('uuid/v4')
+const mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
+
+/* ================================
+ * Constants
+ * ================================
+ */
+const ACCESS_LEVEL = {
+    USER: 10,
+    POWER_USER: 20,
+    SERVICE: 30,
+    MANAGER: 40,
+    ADMIN: 50,
+}
+
+/* ================================
+ * Schema
+ * ================================
+ */
+
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    accessLevel: { type: Number, default: ACCESS_LEVEL.USER },
+    isDefault: { type: Boolean, default: false },
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
+})
+
+const applicationSchema = mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    name: String,
+    description: String,
+    key: String,
+    secret: String,
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
+})
+
+const gatewaySchema = mongoose.Schema({
+    application: { type: mongoose.Schema.Types.ObjectId, ref: 'Application' },
+    name: String,
+    description: String,
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
+})
+
+const deviceSchema = mongoose.Schema({
+    gateway: { type: mongoose.Schema.Types.ObjectId, ref: 'Gateway' },
+    name: String,
+    description: String,
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
+});
+
+/* ================================
+ * Models
+ * ================================
+ */
+const User = mongoose.model('User', userSchema)
+const Application = mongoose.model('Application', applicationSchema)
+const Gateway = mongoose.model('Gateway', gatewaySchema)
+const Device = mongoose.model('Device', deviceSchema)
+
+/* ================================
+ * Database
+ * ================================
+ */
+
+function generatePassword(password) {
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds)
+    const hash = bcrypt.hashSync(password, salt)
+    return hash
+}
+
+function createDefaultUser() {
+    User.findOne({ isDefault: true })
+    .then((res) => {
+        if (!res) {
+            // create new user
+            let defaultUser = new User({
+                firstName: 'Default',
+                lastName: 'User',
+                email: 'admin',                      // default email
+                password: generatePassword('admin'), // default password
+                isDefault: true
+            })
+            defaultUser.save()
+            .then((u) => {
+                logger.info(`Created default user ${u.firstName} ${u.lastName}`)
+            })
+        } else {
+            logger.info(`Default user present`)
+        }
+    })
+    .catch((err) => {
+        logger.error(err.message)
+    })
+}
+
+const connection = function() {
+    return new Promise((fulfill, reject) => {
+        // set up connection
+        mongoose.connect('mongodb://db/openhome', { useMongoClient: true })
+        .then((instance) => {
+            logger.info('Connected to DB')
+            createDefaultUser() // create default user for the first time
+            fulfill(instance)
+        })
+        .catch((err) => {
+            logger.error('MongoDB connection error:', err.message)
+            reject(err)
+        })
+    })
+}
+
+module.exports = {
+    ACCESS_LEVEL,
+    connection,
+    User,
+    Application,
+    Gateway,
+    Device,
+}
