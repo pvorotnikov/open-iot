af5313d641ce79d4451f4ced350687e45cb15134
Petar Vorotnikov <petar@vorotnikov.net>
2017-11-20 20:14:55 +0200

    Removed requirement for republish topic to be different than publish since republishing is made in the scope of an application, while publishing is in the scope of a gateway. They will (almost) never collide

--- helpers/http_publish_test.sh
+++ helpers/http_publish_test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-GATEWAY_ID=5a12cc1b7b115803e1c0fe41
-APP_ID=5a12caab1d285c03d46994ac
-APP_KEY=2ba343c8
-APP_SECRET=c3af48947e4c5323
-
 curl -u $APP_KEY:$APP_SECRET -H "content-type:application/json" -XPOST -d '{"test":123}' http://127.0.0.1:8080/api/publish/$APP_ID/$GATEWAY_ID/test
--- helpers/mqtt_publish_test.sh
+++ helpers/mqtt_publish_test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-GATEWAY_ID=5a1143ee66d6ad00916a3b43
-APP_ID=5a069b2e6ab5320066cc2d6f
-APP_KEY=64ba3f86
-APP_SECRET=decb0af572081bd2
-
-mosquitto_pub -h 127.0.0.1 -p 1883 -u $APP_KEY -P $APP_SECRET -t $APP_ID/$GATEWAY_ID/test_topic -m "test-message"
+mosquitto_pub -h 127.0.0.1 -p 1883 -u $APP_KEY -P $APP_SECRET -t $APP_ID/$GATEWAY_ID/test -m "test-message"
