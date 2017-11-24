5229f0dd5c96e4b545c304d89cde8d37237ae5ed
Petar Vorotnikov <petar@vorotnikov.net>
2017-11-24 10:17:42 +0200

    Moved Dockerfiles from helpers to the respective services. Configured HAProxy to act as SSL Offloading proxy, but still doesn't work  (damn HAProxy)

--- helpers/test_mqtt/test-pub.sh
+++ helpers/test_mqtt/test-pub.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-APP_ID=5a16981e3962fe001e0c2992
-APP_KEY=61964572
-APP_SECRET=872947ac94038e01
+APP_ID=5a17d4cc9912b8001e670ca6
+APP_KEY=d976b8d0
+APP_SECRET=409343564cc8b2e0
 
 mosquitto_pub \
     -h 127.0.0.1 \
-    -p 8883 \
+    -p 1883 \
     -u $APP_KEY \
     -P $APP_SECRET \
     -t $APP_ID/message \
