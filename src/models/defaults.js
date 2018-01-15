c772d2ba54ee0fd1b28d5e2271ae4b29e56feb73
Petar Vorotnikov <petar@vorotnikov.net>
2018-01-15 12:38:57 +0200

    Added proper merging of default settings and new settings

--- src/models/defaults.js
+++ src/models/defaults.js
@@ -10,6 +10,11 @@ const defaultSettings = [
         readOnly: true,
     },
     {
+        key: 'global.aliases',
+        value: false,
+        description: 'Use aliases instead of ids for publishing and subscribing',
+    },
+    {
         key: 'bridge.aws.enabled',
         value: false,
         description: 'Enable AWS IoT bridge',
@@ -42,21 +47,31 @@ const defaultSettings = [
 ]
 
 function settings(Model) {
-    // Model.remove({})
-    // .then(() => Model.findOne())
-    Model.findOne()
+
+    let requiredSettings = defaultSettings.map(s => s.key)
+
+    Model.find({ key: { $in: requiredSettings } })
     .then(res => {
-        if (!res) {
-            logger.info('Crating default settings...')
-            defaultSettings.forEach(s => {
-                logger.info(`${s.key} -> ${s.value}`)
+
+        // walk through all default settings to check if there's entry in the DB
+        defaultSettings.forEach(s => {
+
+            let shouldAdd = true
+            for (let i=0; i < res.length; i++) {
+                if (s.key === res[i].key) {
+                    logger.debug(`Setting ${s.key} already present`)
+                    shouldAdd = false
+                    break
+                }
+            }
+
+            if (shouldAdd) {
+                logger.info(`Creating default setting ${s.key} -> ${s.value}...`)
                 let setting = new Model(s)
                 setting.save()
-            })
+            }
 
-        } else {
-            logger.info(`Settings present`)
-        }
+        })
     })
     .catch(err => {
         logger.error(err.message)
--- src/public/js/src/SettingsPage/SettingsPage.js
+++ src/public/js/src/SettingsPage/SettingsPage.js
@@ -47,6 +47,7 @@ class SettingsPage extends Component {
 
             if (!s.readOnly) {
                 switch (s.key) {
+                    case 'global.aliases':
                     case 'bridge.aws.enabled':
                         valueElement = this.renderEditableToggle(s.key, s.value)
                         break
