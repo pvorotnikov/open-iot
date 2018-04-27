aa0827461a47ea39b7f995b21f1ecdfa5a4f153e
Petar Vorotnikov <petar@vorotnikov.net>
2018-04-27 07:15:59 +0300

    Completed unit tests for defaults

--- test/models/default.spec.js
+++ test/models/default.spec.js
@@ -9,16 +9,16 @@ const expect = chai.expect
 
 const { logger, cleanDb } = require('../_utils')
 const { User, Setting, ACCESS_LEVEL } = require('../../src/models')
-const defaults = require('../../src/models/defaults')
+const defaults = rewire('../../src/models/defaults')
 
 describe('Defaults', function() {
 
-    before(done => {
+    beforeEach(done => {
         cleanDb()
         .then(res => done())
     })
 
-    after(done => {
+    afterEach(done => {
         cleanDb()
         .then(res => done())
     })
@@ -30,32 +30,38 @@ describe('Defaults', function() {
 
     it('should create default user', done => {
         defaults.user(User)
-        .then(() => {
-            return User.find()
-        })
-        .then((users) => {
+        .then(() => User.find())
+        .then(users => {
             users.length.should.equal(1)
             users[0].firstName.should.equal('Default')
             users[0].lastName.should.equal('User')
             users[0].email.should.equal('admin')
             users[0].isDefault.should.equal(true)
             users[0].accessLevel.should.equal(ACCESS_LEVEL.ADMIN)
+            done()
         })
-        .finally(() => done())
     })
 
     it('should not create default user - db error', done => {
 
-        let userFindStub = sinon.stub(User, 'findOne').rejects(new Error('user-rejection'))
+        const userFindStub = sinon.stub(User, 'findOne').rejects(new Error('user-rejection'))
+
+        defaults.user(User)
+        .catch(err => {
+            err.message.should.equal('user-rejection')
+            userFindStub.restore()
+            done()
+        })
+    })
+
+    it('should not create default user - already present', done => {
+        const userSaveSpy = sinon.spy(User.prototype, 'save')
+        const userFindStub = sinon.stub(User, 'findOne').resolves({})
 
         defaults.user(User)
         .then(() => {
-            return User.find()
-        })
-        .catch((err) => {
-            err.message.should.equal('user-rejection')
-        })
-        .finally(() => {
+            userSaveSpy.should.not.have.been.called
+            userSaveSpy.restore()
             userFindStub.restore()
             done()
         })
@@ -64,23 +70,36 @@ describe('Defaults', function() {
     it('should create default settings', done => {
         defaults.settings(Setting)
         .then(() => {
-            return Setting.find()
+            setImmediate(() => {
+                Setting.find()
+                .then(settings => {
+                    settings.length.should.be.gt(0)
+                    done()
+                })
+            })
         })
-        .then((settings) => {
-            logger.info(settings.length)
-        })
-        .finally(() => done())
     })
 
     it('should not create default settings - db error', done => {
 
-        let settingsFindStub = sinon.stub(Setting, 'find').rejects(new Error('settings-rejection'))
+        const settingsFindStub = sinon.stub(Setting, 'find').rejects(new Error('settings-rejection'))
 
         defaults.settings(Setting)
         .catch((err) => {
             err.message.should.equal('settings-rejection')
+            settingsFindStub.restore()
+            done()
         })
-        .finally(() => {
+    })
+
+    it('should not create default settings - already present', done => {
+        const settingsSaveSpy = sinon.spy(Setting.prototype, 'save')
+        const settingsFindStub = sinon.stub(Setting, 'find').resolves(defaults.__get__('defaultSettings'))
+
+        defaults.settings(Setting)
+        .then(() => {
+            settingsSaveSpy.should.not.have.been.called
+            settingsSaveSpy.restore()
             settingsFindStub.restore()
             done()
         })
