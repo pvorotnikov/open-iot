e0b8881f12a4fc36cd1832f158d35fd5b1743164
Petar Vorotnikov <petar@vorotnikov.net>
2019-02-11 08:45:02 +0200

    Created complete unit test suite for gateways

--- src/gateways.js
+++ src/gateways.js
@@ -1,161 +1,165 @@
 const express = require('express')
-const router = express.Router()
 const validator = require('validator')
 const { logger, responses, auth } = require('./lib')
 const { ACCESS_LEVEL, Gateway, Application } = require('./models')
 const { SuccessResponse, ErrorResponse } = responses
 
-// fetch all registered gateways
-router.get('/', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
+module.exports = function(app) {
 
-    Gateway
-    .find()
-    .where('user').eq(req.user._id)
-    .populate('application')
-    .then(gateways => {
-        let data = gateways.map(g => ({
-            id: g.id,
-            name: g.name,
-            alias: g.alias,
-            description: g.description,
-            created: g.created,
-            updated: g.updated,
-            application: {
-                id: g.application.id,
-                name: g.application.name,
-                alias: g.application.alias,
-            },
-        }))
-        res.json(new SuccessResponse(data))
-    })
-    .catch((err) => {
-        res.status(500).json(new ErrorResponse(err.message))
-    })
+    const router = express.Router()
+    app.use('/api/gateways', router)
 
-})
+    // fetch all registered gateways
+    router.get('/', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
 
-// fetch gateway by id that belongs to a particular user
-router.get('/:id', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
-
-    Gateway
-    .findById(req.params.id)
-    .where('user').eq(req.user._id)
-    .then((g) => {
-        if (g) {
-            let data = {
+        Gateway
+        .find()
+        .where('user').eq(req.user._id)
+        .populate('application')
+        .then(gateways => {
+            let data = gateways.map(g => ({
                 id: g.id,
                 name: g.name,
                 alias: g.alias,
                 description: g.description,
                 created: g.created,
                 updated: g.updated,
+                application: {
+                    id: g.application.id,
+                    name: g.application.name,
+                    alias: g.application.alias,
+                },
+            }))
+            res.json(new SuccessResponse(data))
+        })
+        .catch((err) => {
+            res.status(500).json(new ErrorResponse(err.message))
+        })
+
+    })
+
+    // fetch gateway by id that belongs to a particular user
+    router.get('/:id', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
+
+        Gateway
+        .findById(req.params.id)
+        .where('user').eq(req.user._id)
+        .then((g) => {
+            if (g) {
+                let data = {
+                    id: g.id,
+                    name: g.name,
+                    alias: g.alias,
+                    description: g.description,
+                    created: g.created,
+                    updated: g.updated,
+                }
+                res.json({ status: 'ok', data })
+            } else {
+                res.status(400).json(new ErrorResponse('Gateway not found'))
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(new ErrorResponse(err.message))
+        })
+
+    })
+
+    // create new gateway
+    router.post('/', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
+
+        const { application, name, description } = req.body
+
+        if (validator.isEmpty(application)) {
+            return res.status(400).json(new ErrorResponse('You need to specify a parent application'))
+        }
+
+        if (validator.isEmpty(name)) {
+            return res.status(400).json(new ErrorResponse('Please, enter gateway name'))
+        }
+
+        if (validator.isEmpty(description)) {
+            return res.status(400).json(new ErrorResponse('Please, enter gateway description'))
+        }
+
+        // check the owner of the application
+        Application.findById(application)
+        .where('user').eq(req.user._id)
+        .then(app => {
+
+            if (!app) {
+                throw new Error('This application belongs to somebody else')
+            }
+
+            let gateway = new Gateway({
+                user: req.user._id,
+                application,
+                name,
+                alias: name.toLowerCase().replace(/\s/g, ''),
+                description,
+            })
+            return gateway.save()
+        })
+        .then(gateway => {
+            let data = {
+                id: gateway.id,
+                name: gateway.name,
+                alias: gateway.alias,
+                description: gateway.description,
+                created: gateway.created,
+                updated: gateway.updated,
             }
             res.json({ status: 'ok', data })
-        } else {
-            res.status(400).json(new ErrorResponse('Gateway not found'))
-        }
-    })
-    .catch((err) => {
-        res.status(500).json(new ErrorResponse(err.message))
-    })
-
-})
-
-// create new gateway
-router.post('/', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
-
-    const { application, name, description } = req.body
-
-    if (validator.isEmpty(application)) {
-        return res.status(400).json(new ErrorResponse('You need to specify a parent application'))
-    }
-
-    if (validator.isEmpty(name)) {
-        return res.status(400).json(new ErrorResponse('Please, enter gateway name'))
-    }
-
-    if (validator.isEmpty(description)) {
-        return res.status(400).json(new ErrorResponse('Please, enter gateway description'))
-    }
-
-    // check the owner of the application
-    Application.findById(application)
-    .where('user').eq(req.user._id)
-    .then(app => {
-
-        if (!app) {
-            throw new Error('This application belongs to somebody else')
-        }
-
-        let gateway = new Gateway({
-            user: req.user._id,
-            application,
-            name,
-            alias: name.toLowerCase().replace(/\s/g, ''),
-            description,
         })
-        return gateway.save()
+        .catch(err => {
+            res.status(500).json(new ErrorResponse(err.message))
+        })
+
     })
-    .then(gateway => {
-        let data = {
-            id: gateway.id,
-            name: gateway.name,
-            alias: gateway.alias,
-            description: gateway.description,
-            created: gateway.created,
-            updated: gateway.updated,
+
+    // update gateway
+    router.put('/:id', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
+
+        const { name, description, alias } = req.body
+
+        const updateDefintion = {}
+
+        if (name && !validator.isEmpty(name)) {
+            updateDefintion.name = name
         }
-        res.json({ status: 'ok', data })
-    })
-    .catch(err => {
-        res.status(500).json(new ErrorResponse(err.message))
-    })
 
-})
+        if (alias && !validator.isEmpty(alias)) {
+            updateDefintion.alias = alias.toLowerCase().replace(/\s/g, '')
+        }
 
-// update gateway
-router.put('/:id', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
+        if (description && !validator.isEmpty(description)) {
+            updateDefintion.description = description
+        }
 
-    const { name, description, alias } = req.body
+        Gateway.findByIdAndUpdate(req.params.id, updateDefintion)
+        .where('user').eq(req.user._id)
+        .then(() => {
+            res.json(new SuccessResponse())
+        })
+        .catch(err => {
+            res.status(500).json(new ErrorResponse(err.message))
+        })
 
-    const updateDefintion = {}
-
-    if (name && !validator.isEmpty(name)) {
-        updateDefintion.name = name
-    }
-
-    if (alias && !validator.isEmpty(alias)) {
-        updateDefintion.alias = alias.toLowerCase().replace(/\s/g, '')
-    }
-
-    if (description && !validator.isEmpty(description)) {
-        updateDefintion.description = description
-    }
-
-    Gateway.findByIdAndUpdate(req.params.id, updateDefintion)
-    .where('user').eq(req.user._id)
-    .then(() => {
-        res.json(new SuccessResponse())
-    })
-    .catch(err => {
-        res.status(500).json(new ErrorResponse(err.message))
     })
 
-})
+    // delete gateway
+    router.delete('/:id', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
 
-// delete gateway
-router.delete('/:id', auth.protect(ACCESS_LEVEL.USER), (req, res, next) => {
+        Gateway.findById(req.params.id)
+        .where('user').eq(req.user._id)
+        .then(gateway => gateway.remove())
+        .then(() => {
+            res.json(new SuccessResponse())
+        })
+        .catch(err => {
+            res.status(500).json(new ErrorResponse(err.message))
+        })
 
-    Gateway.findById(req.params.id)
-    .where('user').eq(req.user._id)
-    .then(gateway => gateway.remove())
-    .then(() => {
-        res.json(new SuccessResponse())
     })
-    .catch(err => {
-        res.status(500).json(new ErrorResponse(err.message))
-    })
 
-})
-
-module.exports = router
+}
---
+++ test/gateways.spec.js
@@ -0,0 +1,401 @@
+const _ = require('lodash')
+const nconf = require('nconf')
+const Promise = require('bluebird')
+const chai = require('chai')
+const request = require('supertest')
+const sinon = require('sinon')
+const should = chai.should()
+const expect = chai.expect
+const moment = require('moment')
+const rewire = require('rewire')
+
+const { cleanDb, expressApp, objectId, } = require('./_utils')
+
+const { utils } = require('../src/lib')
+const { User, Gateway, Application, ACCESS_LEVEL } = require('../src/models')
+const gateways = rewire('../src/gateways')
+
+describe('Gateways', function() {
+
+    const app = expressApp([gateways])
+    let userAuthorization, adminAuthorization
+
+    async function createUsers() {
+        const users = await User.insertMany([
+            { firstName: 'User', lastName: 'User', email: 'user', password: utils.generatePassword('user'), },
+            { firstName: 'Admin', lastName: 'Admin', email: 'admin', password: utils.generatePassword('admin'), accessLevel: ACCESS_LEVEL.ADMIN, },
+        ])
+        const [user, admin] = users
+        userAuthorization = 'Basic ' + Buffer.from(user.key + ':' + user.secret).toString('base64')
+        adminAuthorization = 'Basic ' + Buffer.from(admin.key + ':' + admin.secret).toString('base64')
+    }
+
+    function fakeGateway() {
+        return {
+            id: 'gwid',
+            name: 'gwname',
+            description: 'gwdescription',
+            alias: 'gwalias',
+            created: moment().toISOString(),
+            updated: moment().toISOString(),
+            application: {
+                id: 'appid',
+                name: 'appname',
+                alias: 'appalias',
+                created: moment().toISOString(),
+                updated: moment().toISOString(),
+            },
+            remove: sinon.stub().resolves(),
+        }
+    }
+
+    before(async () => {
+        await cleanDb()
+        await createUsers()
+    })
+
+    after(async () => {
+        await cleanDb()
+    })
+
+    /* ============================
+     * GET GATEWAYS
+     * ============================
+     */
+
+    describe('Get gateways', function() {
+
+        it('should get all gateways', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'find').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().returnsThis(),
+                populate: sinon.stub().resolves([fakeGateway()]),
+            })
+
+            const res = await request(app)
+            .get('/api/gateways')
+            .set('Authorization', userAuthorization)
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(200)
+            res.body.data.should.be.an('array')
+            res.body.data.length.should.equal(1)
+        })
+
+        it('should not all gateways - db error', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'find').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().returnsThis(),
+                populate: sinon.stub().rejects(new Error('DB Error')),
+            })
+
+            const res = await request(app)
+            .get('/api/gateways')
+            .set('Authorization', userAuthorization)
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(500)
+            res.body.status.should.equal('error')
+            res.body.errorMessage.should.equal('DB Error')
+        })
+
+    })
+
+    /* ============================
+     * GET A GATEWAY
+     * ============================
+     */
+
+    describe('Get a gateway', function() {
+
+        it('should get a gateway', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'findById').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().resolves(fakeGateway()),
+            })
+
+            const res = await request(app)
+            .get('/api/gateways/123')
+            .set('Authorization', userAuthorization)
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(200)
+            res.body.data.should.be.an('object')
+            res.body.data.id.should.equal('gwid')
+        })
+
+        it('should not get a gateway - does not exist', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'findById').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().resolves(null),
+            })
+
+            const res = await request(app)
+            .get('/api/gateways/123')
+            .set('Authorization', userAuthorization)
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(400)
+        })
+
+        it('should not get a gateway - DB Error', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'findById').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().rejects(new Error('DB Error')),
+            })
+
+            const res = await request(app)
+            .get('/api/gateways/123')
+            .set('Authorization', userAuthorization)
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(500)
+            res.body.status.should.equal('error')
+            res.body.errorMessage.should.equal('DB Error')
+        })
+
+    })
+
+    /* ============================
+     * CREATE A GATEWAY
+     * ============================
+     */
+
+    describe('Create a gateway', function() {
+
+        const GatewayModel = gateways.__get__('Gateway')
+
+        afterEach(() => {
+            gateways.__set__('Gateway', GatewayModel)
+        })
+
+        it('should create a gateway', async () => {
+
+            // create stubs
+            let applicationStub = sinon.stub(Application, 'findById').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().resolves({_id: objectId() }),
+            })
+
+            const res = await request(app)
+            .post('/api/gateways')
+            .set('Authorization', userAuthorization)
+            .send({ application: objectId(), name: 'gateway', description: 'gateway' })
+
+            // restore stubs
+            applicationStub.restore()
+
+            res.status.should.equal(200)
+            res.body.data.should.be.an('object')
+        })
+
+        it('should not create a gateway - DB error', async () => {
+
+            // create stubs
+            let applicationStub = sinon.stub(Application, 'findById').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().rejects(new Error('DB Error')),
+            })
+
+            const res = await request(app)
+            .post('/api/gateways')
+            .set('Authorization', userAuthorization)
+            .send({ application: objectId(), name: 'gateway', description: 'gateway' })
+
+            // restore stubs
+            applicationStub.restore()
+
+            res.status.should.equal(500)
+            res.body.status.should.equal('error')
+            res.body.errorMessage.should.equal('DB Error')
+        })
+
+        it('should not create a gateway - wrong application', async () => {
+
+            // create stubs
+            let applicationStub = sinon.stub(Application, 'findById').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().resolves(null),
+            })
+
+            const res = await request(app)
+            .post('/api/gateways')
+            .set('Authorization', userAuthorization)
+            .send({ application: objectId(), name: 'gateway', description: 'gateway' })
+
+            // restore stubs
+            applicationStub.restore()
+
+            res.status.should.equal(500)
+        })
+
+        it('should create a gateway - empty application', async () => {
+
+            const res = await request(app)
+            .post('/api/gateways')
+            .set('Authorization', userAuthorization)
+            .send({ application: '', name: 'gateway', description: 'gateway' })
+
+            res.status.should.equal(400)
+        })
+
+        it('should create a gateway - empty name', async () => {
+
+            const res = await request(app)
+            .post('/api/gateways')
+            .set('Authorization', userAuthorization)
+            .send({ application: objectId(), name: '', description: 'gateway' })
+
+            res.status.should.equal(400)
+        })
+
+        it('should create a gateway - empty description', async () => {
+
+            const res = await request(app)
+            .post('/api/gateways')
+            .set('Authorization', userAuthorization)
+            .send({ application: objectId(), name: 'gateway', description: '' })
+
+            res.status.should.equal(400)
+        })
+
+    })
+
+    /* ============================
+     * UPDATE A GATEWAY
+     * ============================
+     */
+
+    describe('Update a gateway', function() {
+
+        it('should update a gateway', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'findByIdAndUpdate').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().resolves(fakeGateway()),
+            })
+
+            const res = await request(app)
+            .put('/api/gateways/123')
+            .set('Authorization', userAuthorization)
+            .send({ name: 'gateway', description: 'gateway', alias: 'gateway' })
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(200)
+        })
+
+        it('should update a gateway - no change', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'findByIdAndUpdate').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().resolves(fakeGateway()),
+            })
+
+            const res = await request(app)
+            .put('/api/gateways/123')
+            .set('Authorization', userAuthorization)
+            .send({})
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(200)
+        })
+
+        it('should not update a gateway - DB Error', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'findByIdAndUpdate').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().rejects(new Error('DB Error')),
+            })
+
+            const res = await request(app)
+            .put('/api/gateways/123')
+            .set('Authorization', userAuthorization)
+            .send({})
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(500)
+            res.body.status.should.equal('error')
+            res.body.errorMessage.should.equal('DB Error')
+        })
+
+    })
+
+    /* ============================
+     * REMOVE A GATEWAY
+     * ============================
+     */
+
+    describe('Remove a gateway', function() {
+
+        it('should remove a gateway', async () => {
+
+            // create stubs
+            const gw = fakeGateway()
+            let gatewayStub = sinon.stub(Gateway, 'findById').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().resolves(gw),
+            })
+
+            const res = await request(app)
+            .delete('/api/gateways/123')
+            .set('Authorization', userAuthorization)
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(200)
+            gw.remove.should.have.been.called
+        })
+
+        it('should not remove a gateway - DB Error', async () => {
+
+            // create stubs
+            let gatewayStub = sinon.stub(Gateway, 'findById').returns({
+                where: sinon.stub().returnsThis(),
+                eq: sinon.stub().rejects(new Error('DB Error')),
+            })
+
+            const res = await request(app)
+            .delete('/api/gateways/123')
+            .set('Authorization', userAuthorization)
+
+            // restore stubs
+            gatewayStub.restore()
+
+            res.status.should.equal(500)
+            res.body.status.should.equal('error')
+            res.body.errorMessage.should.equal('DB Error')
+        })
+
+    })
+
+})
